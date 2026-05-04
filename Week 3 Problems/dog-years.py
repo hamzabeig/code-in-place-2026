31c54abbe90b72076afad522229f34e2704e3796
Hamza Baig <161630144+hamzabeig@users.noreply.github.com>
2026-05-04 11:59:35 +0500

    Create dog-years.py

---
+++ Week 3 Problems/dog-years.py
@@ -0,0 +1,11 @@
+# Each year for a human is like 7.18 years for a dog
+DOG_YEARS_MULTIPLIER = 7.18  
+
+def main():
+    dog_age = int(input("Enter an age in calendar years: "))
+    print(f"That's {dog_age*DOG_YEARS_MULTIPLIER} in dog years!")
+
+
+# There is no need to edit code beyond this point
+if __name__ == '__main__':
+    main()
